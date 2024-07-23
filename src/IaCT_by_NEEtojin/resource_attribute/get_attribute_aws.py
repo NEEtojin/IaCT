f32fea07f55032cb1afea1b05861176f63c384b2
NEEtojin <neetojin@outlook.kr>
2024-07-23 20:10:26 +0900

    added get_attribute_aws.py

---
+++ src/IaCT_by_NEEtojin/resource_attribute/get_attribute_aws.py
@@ -0,0 +1,12 @@
+"""
+(.*">)([a-z0-9]+)(</a>":.*)
+(.*getatt.*"code">)([a-z0-9]+)(</code>.*)
+process html
+replace with regex
+"""
+
+def get_attribute():
+    print("IaCT")
+
+if __name__ == "__main__":
+    get_attribute()
