a0bf835de98da26c9b6d9143c8511f631444fdce
NEEtojin <neetojin@outlook.kr>
2024-07-24 20:06:32 +0900

    tested regex for properties

--- src/IaCT_by_NEEtojin/resource_attribute/get_attribute_aws.py
+++ src/IaCT_by_NEEtojin/resource_attribute/get_attribute_aws.py
@@ -4,9 +4,17 @@
 process html
 replace with regex
 """
+import re
+import urllib.request
 
 def get_attribute():
     print("IaCT")
+    with urllib.request.urlopen("https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-resource-ec2-instance.html") as response:
+        html=response.read()
+        count = 0
+        for property in re.findall(r'(.*">)([A-Za-z0-9]+)(</a>" : .*)', html.decode('utf-8')):
+            print(count, property[1])
+            count+=1
 
 if __name__ == "__main__":
     get_attribute()
