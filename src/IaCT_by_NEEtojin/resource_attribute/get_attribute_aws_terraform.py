7cbf524d8e83d41a9d2359137ac5cd2f23fcd8b8
NEEtojin <neetojin@outlook.kr>
2024-07-27 19:39:00 +0900

    testing extraction of resource url

--- src/IaCT_by_NEEtojin/resource_attribute/get_attribute_aws_terraform.py
+++ src/IaCT_by_NEEtojin/resource_attribute/get_attribute_aws_terraform.py
@@ -2,7 +2,12 @@ import re
 import urllib.request
 
 def get_service():
-    pass
+    url = f"https://github.com/hashicorp/terraform-provider-aws/tree/main/website/docs/d"
+    with urllib.request.urlopen(url) as response:
+        html = response.read()
+        #print(html.decode('utf-8'))
+        for resource in re.findall(r'("name":")([a-z0-9_]+)(\.html\.markdown")', html.decode('utf-8')):
+            print(resource[1], end=" ")
 
 def get_resource(service: str = "ec2"):
     get_attribute(service)
@@ -18,4 +23,5 @@ def get_attribute(resource: str = "instance"):
             print(argument[1])
 
 if __name__ == "__main__" :
-    get_attribute()
+    #get_attribute()
+    get_service()
